0e7b1d4abd2044f95bbd4e526363f6f5ba607f4e
andygeiss <4ndygeiss@gmail.com>
2024-09-16 09:31:32 +0200

    fix: only control inputs influence the player velocity

--- systems/movement.go
+++ systems/movement.go
@@ -11,32 +11,34 @@ type movementSystem struct{}
 func (a *movementSystem) Process(em ecs.EntityManager) (state int) {
 	controls := em.Get("controls")
 	controlsState := controls.Get(components.MaskState).(*components.State)
+	// Set the velocity based on the player's input.
+	if controlsState.Value > 0 {
+		velocity := em.Get("player").Get(components.MaskVelocity).(*components.Velocity)
+		if controlsState.HasState(components.StateControlsW) {
+			velocity.X = 0
+			velocity.Y = -100
+		}
+		if controlsState.HasState(components.StateControlsA) {
+			velocity.X = -100
+			velocity.Y = 0
+		}
+		if controlsState.HasState(components.StateControlsS) {
+			velocity.X = 0
+			velocity.Y = 100
+		}
+		if controlsState.HasState(components.StateControlsD) {
+			velocity.X = 100
+			velocity.Y = 0
+		}
+	} else {
+		velocity := em.Get("player").Get(components.MaskVelocity).(*components.Velocity)
+		velocity.X = 0
+		velocity.Y = 0
+	}
+	// Calculate the next position of the sprites.
 	for _, e := range em.FilterByMask(components.MaskPosition | components.MaskVelocity) {
 		position := e.Get(components.MaskPosition).(*components.Position)
 		velocity := e.Get(components.MaskVelocity).(*components.Velocity)
-		// Set the velocity based on the player's input.
-		if controlsState.Value > 0 {
-			if controlsState.HasState(components.StateControlsW) {
-				velocity.X = 0
-				velocity.Y = -100
-			}
-			if controlsState.HasState(components.StateControlsA) {
-				velocity.X = -100
-				velocity.Y = 0
-			}
-			if controlsState.HasState(components.StateControlsS) {
-				velocity.X = 0
-				velocity.Y = 100
-			}
-			if controlsState.HasState(components.StateControlsD) {
-				velocity.X = 100
-				velocity.Y = 0
-			}
-		} else {
-			velocity.X = 0
-			velocity.Y = 0
-		}
-		// Calculate the next position of the sprite.
 		position.X += velocity.X * rl.GetFrameTime()
 		position.Y += velocity.Y * rl.GetFrameTime()
 	}
